2fb17fce29dfeefeeafb37d86144b7a97d5f35c4
sammy1-com <njugunas357@gmail.com>
2026-06-08 09:32:46 +0300

    Delete test_db.php

--- test_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-// WEEK 1 - Database Connection Test
-require_once 'includes/db.php';
-
-echo "<h2>PitStop Parts - DB Test</h2>";
-echo "<p>Host: localhost | Database: pitstop | User: root</p>";
-?>
