44aed81c45cd530558948326c6cf87c724e9d7ab
sammy1-com <njugunas357@gmail.com>
2026-06-25 14:31:16 +0300

    Change admin access requirement for product deletion

--- admin/delete-product.php
+++ admin/delete-product.php
@@ -2,19 +2,15 @@
 if (session_status() === PHP_SESSION_NONE) session_start();
 require_once '../includes/db.php';
 require_once '../includes/functions.php';
-require_admin();
-
+require_manager_or_above();
 $id = (int)($_GET['id'] ?? 0);
 if (!$id) redirect('/admin/products.php');
-
 $result = $conn->query("SELECT image_url FROM products WHERE id = $id");
 $product = $result->fetch_assoc();
-
 if ($product) {
     if ($product['image_url'] && strpos($product['image_url'], 'uploads/parts/') === 0 && file_exists('../' . $product['image_url'])) {
         unlink('../' . $product['image_url']);
     }
     $conn->query("DELETE FROM products WHERE id = $id");
 }
-
 redirect('/admin/products.php');
